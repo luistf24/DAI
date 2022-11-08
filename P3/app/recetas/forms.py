9a7b0d629b55761431a307d409f4805aa5a2375f
luistf24 <luis.tormo24@gmail.com>
2022-11-08 12:41:18 +0100

    Esto se me había olvidado

---
+++ P3/app/recetas/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Receta
+
+
+class RecetaForm(forms.ModelForm):
+
+    class Meta:
+        model = Receta
+        fields  = ('nombre', 'preparación', 'fotos',)
